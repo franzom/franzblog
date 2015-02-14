8f024cdeb7a8e908de6f380639f93d76d3b257e2
franzom <franzom@gmail.com>
2015-02-15 00:24:53 +0100

    social icons en header

--- wp-content/themes/mesocolumn-child/header.php
+++ wp-content/themes/mesocolumn-child/header.php
@@ -109,7 +109,14 @@ if( $get_header_logo  ) { ?>
 <<?php if( !is_singular() || is_page_template('page-templates/template-blog.php') ){ echo 'h1 '; } else { echo 'div '; } ?>><a href="<?php echo home_url( '/' ); ?>" title="<?php echo bloginfo('name'); ?>" rel="home"><?php bloginfo( 'name' ); ?></a><<?php if( !is_singular() || is_page_template('page-templates/template-blog.php') ){ echo '/h1 '; } else { echo '/div '; } ?>><p id="site-description"><?php echo bloginfo('description'); ?></p>
 </span>
 <?php } else { ?>
-<<?php if( !is_singular() || is_page_template('page-templates/template-blog.php') ){ echo 'h1 '; } else { echo 'div '; } ?>><a href="<?php echo home_url( '/' ); ?>" title="<?php echo bloginfo('name'); ?>" rel="home"><?php bloginfo( 'name' ); ?></a><<?php if( !is_singular() || is_page_template('page-templates/template-blog.php') ){ echo '/h1 '; } else { echo '/div '; } ?>><p id="site-description"><?php echo bloginfo('description'); ?></p>
+<<?php if( !is_singular() || is_page_template('page-templates/template-blog.php') ){ echo 'h1 '; } else { echo 'div '; } ?>><a href="<?php echo home_url( '/' ); ?>" title="<?php echo bloginfo('name'); ?>" rel="home"><?php bloginfo( 'name' ); ?></a><<?php if( !is_singular() || is_page_template('page-templates/template-blog.php') ){ echo '/h1 '; } else { echo '/div '; } ?>>
+<!-- AGREGADO BOTONES REDES SOCIALES -->
+<div class="socialIcons">
+	<a href="#" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/fb-r.png"></a>
+	<a href="#" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/tw-r.png"></a>
+	<a href="#" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/in-r.png"></a>
+</div>
+<p id="site-description"><?php echo bloginfo('description'); ?></p>
 <?php } ?>
 
 </div>
